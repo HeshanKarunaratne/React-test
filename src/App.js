a927bfbfc33ee88356ee5d2e9bcb6a277b2eeb00
Heshan Karuaratne <heshan.karunaratne@axiatadigitallabs.com>
2021-03-23 11:53:43 +0530

    adding till logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Header from "./components/Header/index";
+import "./App.scss";
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>App</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
